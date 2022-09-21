95f174b029a4b0f4bb5dcf7a6e50546906d1e67a
guilhermefrag <guilhermefragnani@icloud.com>
2022-09-21 14:06:50 -0300

    subindo DAOs Vazias

---
+++ src/main/java/com/estruturadados/academia/database/dao/FaturaMatriculaDAO.java
@@ -0,0 +1,33 @@
+package com.estruturadados.academia.database.dao;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+public class FaturaMatriculaDAO extends SistemaDAO{
+    
+    @Override
+    public long Delete(Object param) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int Insert(Object param) throws SQLException {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public List<Object> Select() throws SQLException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public long Update(Object param) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+}
---
+++ src/main/java/com/estruturadados/academia/database/dao/MatriculaModalidadeDAO.java
@@ -0,0 +1,31 @@
+package com.estruturadados.academia.database.dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class MatriculaModalidadeDAO extends SistemaDAO{
+    
+    @Override
+    public long Delete(Object param) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int Insert(Object param) throws SQLException {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public List<Object> Select() throws SQLException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public long Update(Object param) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+}
