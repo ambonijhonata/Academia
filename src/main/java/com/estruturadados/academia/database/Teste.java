18a14d5134efeb3995cf3b50b182be3c6dbac1f2
Adair Locks Grassi <adairlocksgrassi@gmail.com>
2022-10-04 20:07:35 -0300

    implementando lista

--- src/main/java/com/estruturadados/academia/database/Teste.java
+++ src/main/java/com/estruturadados/academia/database/Teste.java
@@ -1,50 +1,38 @@
 package com.estruturadados.academia.database;
 
+import com.estruturadados.academia.database.dao.AlunoDAO;
+import com.estruturadados.academia.database.dao.CidadeDAO;
+import com.estruturadados.academia.database.model.Aluno;
+import com.estruturadados.academia.database.model.Cidade;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
 
-import com.estruturadados.academia.database.dao.AlunoDAO;
-import com.estruturadados.academia.database.model.Aluno;
-import com.estruturadados.academia.database.dao.CidadeDAO;
-import com.estruturadados.academia.database.model.Cidade;
-
 public class Teste {
 
-	public static void main(String[] args) throws SQLException {
-		
-		Connection connection = ConnectionFactory.getConnection
-								(
-									"localhost", 
-									"5432", 
-									"sensei_academia", 
-									"postgres", 
-									"123"
-								);
-		if (connection != null) {
-			System.out.println("CONECTADO");
-			
-			AlunoDAO alunoDAO = new AlunoDAO(connection);
-            CidadeDAO cidadeDAO = new CidadeDAO(connection);
-			
-			/*Pessoa p = new Pessoa();
-			p.setNome("Junior");
-			p.setCpf("789");
-			
-			int fezInsert = dao.Insert(p);
-			
-			System.out.println(fezInsert);*/
-			
-			List<Object> listaAluno = alunoDAO.Select();
-            List<Object> listaCidade = cidadeDAO.Select();
-			
-			
-			
-		} 
-		else {
-			System.out.println("NÃO CONECTADO");
-		}
+  public static void main(String[] args) throws SQLException {
+    Connection connection = ConnectionFactory.getConnection(
+      "localhost",
+      "5432",
+      "academia",
+      "postgres",
+      "postgres"
+    );
+    if (connection != null) {
+      System.out.println("CONECTADO");
 
-	}
+      AlunoDAO alunoDAO = new AlunoDAO(connection);
+      CidadeDAO cidadeDAO = new CidadeDAO(connection);
 
+      List<Object> listaAluno = alunoDAO.Select();
+
+      for (Object o : listaAluno) {
+        System.out.println(((Aluno) o).getAluno());
+      }
+      //   List<Object> listaCidade = cidadeDAO.Select();
+
+    } else {
+      System.out.println("NÃO CONECTADO");
+    }
+  }
 }
--- src/main/java/com/estruturadados/academia/database/model/Aluno.java
+++ src/main/java/com/estruturadados/academia/database/model/Aluno.java
@@ -3,199 +3,212 @@ package com.estruturadados.academia.database.model;
 import java.util.Date;
 import java.util.List;
 
-
 public class Aluno {
 
-	private int codigoAluno;
-	private String aluno;
-	private Date dataNascimento;
-	private char sexo;
-    private String telefone;
-    private String celular;
-    private String email;
-    private String observacao;
-    private String endereco;
-    private String numero;
-    private String complemento;
-    private String bairro;
-    private String cidade;
-    private String estado;
-    private String pais;
-    private String cep;
+  private int codigoAluno;
+  private String aluno;
+  private Date dataNascimento;
+  private char sexo;
+  private String telefone;
+  private String celular;
+  private String email;
+  private String observacao;
+  private String endereco;
+  private String numero;
+  private String complemento;
+  private String bairro;
+  private String cidade;
+  private String estado;
+  private String pais;
+  private String cep;
 
-	public Aluno() {
-		
-	}
+  public Aluno() {}
 
-    public Aluno(int codigo_aluno, String aluno, Date data_nascimento, char sexo, String telefone, String celular, String email, String observacao, String endereco, String numero, String complemento, String bairro, String cidade, String estado, String pais, String cep) {
-        this.codigoAluno = codigo_aluno;
-        this.aluno = aluno;
-        this.dataNascimento = data_nascimento;
-        this.sexo = sexo;
-        this.telefone = telefone;
-        this.celular = celular;
-        this.email = email;
-        this.observacao = observacao;
-        this.endereco = endereco;
-        this.numero = numero;
-        this.complemento = complemento;
-        this.bairro = bairro;
-        this.cidade = cidade;
-        this.estado = estado;
-        this.pais = pais;
-        this.cep = cep;
+  public Aluno(
+    int codigo_aluno,
+    String aluno,
+    Date data_nascimento,
+    Character sexo,
+    String telefone,
+    String celular,
+    String email,
+    String observacao,
+    String endereco,
+    String numero,
+    String complemento, 
+    String bairro,
+    String cidade,
+    String estado,
+    String pais,
+    String cep
+  ) {
+    this.codigoAluno = codigo_aluno;
+    this.aluno = aluno;
+    this.dataNascimento = data_nascimento;
+    this.sexo = sexo;
+    this.telefone = telefone;
+    this.celular = celular;
+    this.email = email;
+    this.observacao = observacao;
+    this.endereco = endereco;
+    this.numero = numero;
+    this.complemento = complemento;
+    this.bairro = bairro;
+    this.cidade = cidade;
+    this.estado = estado;
+    this.pais = pais;
+    this.cep = cep;
+  }
+
+  public int getCodigoAluno() {
+    return codigoAluno;
+  }
+
+  public void setCodigoAluno(int codigo_aluno) {
+    this.codigoAluno = codigo_aluno;
+  }
+
+  public String getAluno() {
+    return aluno;
+  }
+
+  public void setAluno(String aluno) {
+    this.aluno = aluno;
+  }
+
+  public Date getDataNascimento() {
+    return dataNascimento;
+  }
+
+  public void setDataNascimento(Date data_nascimento) {
+    this.dataNascimento = data_nascimento;
+  }
+
+  public char getSexo() {
+    return sexo;
+  }
+
+  public void setSexo(char sexo) {
+    this.sexo = sexo;
+  }
+
+  public String getTelefone() {
+    return telefone;
+  }
+
+  public void setTelefone(String telefone) {
+    this.telefone = telefone;
+  }
+
+  public String getCelular() {
+    return celular;
+  }
+
+  public void setCelular(String celular) {
+    this.celular = celular;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getObservacao() {
+    return observacao;
+  }
+
+  public void setObservacao(String observacao) {
+    this.observacao = observacao;
+  }
+
+  public String getEndereco() {
+    return endereco;
+  }
+
+  public void setEndereco(String endereco) {
+    this.endereco = endereco;
+  }
+
+  public String getNumero() {
+    return numero;
+  }
+
+  public void setNumero(String numero) {
+    this.numero = numero;
+  }
+
+  public String getComplemento() {
+    return complemento;
+  }
+
+  public void setComplemento(String complemento) {
+    this.complemento = complemento;
+  }
+
+  public String getBairro() {
+    return bairro;
+  }
+
+  public void setBairro(String bairro) {
+    this.bairro = bairro;
+  }
+
+  public String getCidade() {
+    return cidade;
+  }
+
+  public void setCidade(String cidade) {
+    this.cidade = cidade;
+  }
+
+  public String getEstado() {
+    return estado;
+  }
+
+  public void setEstado(String estado) {
+    this.estado = estado;
+  }
+
+  public String getPais() {
+    return pais;
+  }
+
+  public void setPais(String pais) {
+    this.pais = pais;
+  }
+
+  public String getCep() {
+    return cep;
+  }
+
+  public void setCep(String cep) {
+    this.cep = cep;
+  }
+
+  public void ImprimeAluno(List<Aluno> listaAluno) {
+    for (int i = 0; i < listaAluno.size(); i++) {
+      Aluno p = (Aluno) listaAluno.get(i);
+      System.out.println(p.getAluno());
+      System.out.println(p.getDataNascimento());
+      System.out.println(p.getSexo());
+      System.out.println(p.getTelefone());
+      System.out.println(p.getCelular());
+      System.out.println(p.getEmail());
+      System.out.println(p.getObservacao());
+      System.out.println(p.getEndereco());
+      System.out.println(p.getNumero());
+      System.out.println(p.getComplemento());
+      System.out.println(p.getBairro());
+      System.out.println(p.getCidade());
+      System.out.println(p.getEstado());
+      System.out.println(p.getPais());
+      System.out.println(p.getCep());
+      System.out.println("====================");
+      //"(aluno,data_nascimento,sexo,telefone,celular,email,observacao,endereco,numero,complemento,bairro,cidade,estado,pais,cep)
+
     }
-
-    public int getCodigoAluno() {
-        return codigoAluno;
-    }
-
-    public void setCodigoAluno(int codigo_aluno) {
-        this.codigoAluno = codigo_aluno;
-    }
-
-    public String getAluno() {
-        return aluno;
-    }
-
-    public void setAluno(String aluno) {
-        this.aluno = aluno;
-    }
-
-    public Date getDataNascimento() {
-        return dataNascimento;
-    }
-
-    public void setDataNascimento(Date data_nascimento) {
-        this.dataNascimento = data_nascimento;
-    }
-
-    public char getSexo() {
-        return sexo;
-    }
-
-    public void setSexo(char sexo) {
-        this.sexo = sexo;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-
-    public String getCelular() {
-        return celular;
-    }
-
-    public void setCelular(String celular) {
-        this.celular = celular;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getObservacao() {
-        return observacao;
-    }
-
-    public void setObservacao(String observacao) {
-        this.observacao = observacao;
-    }
-
-    public String getEndereco() {
-        return endereco;
-    }
-
-    public void setEndereco(String endereco) {
-        this.endereco = endereco;
-    }
-
-    public String getNumero() {
-        return numero;
-    }
-
-    public void setNumero(String numero) {
-        this.numero = numero;
-    }
-
-    public String getComplemento() {
-        return complemento;
-    }
-
-    public void setComplemento(String complemento) {
-        this.complemento = complemento;
-    }
-
-    public String getBairro() {
-        return bairro;
-    }
-
-    public void setBairro(String bairro) {
-        this.bairro = bairro;
-    }
-
-    public String getCidade() {
-        return cidade;
-    }
-
-    public void setCidade(String cidade) {
-        this.cidade = cidade;
-    }
-
-    public String getEstado() {
-        return estado;
-    }
-
-    public void setEstado(String estado) {
-        this.estado = estado;
-    }
-
-    public String getPais() {
-        return pais;
-    }
-
-    public void setPais(String pais) {
-        this.pais = pais;
-    }
-
-    public String getCep() {
-        return cep;
-    }
-
-    public void setCep(String cep) {
-        this.cep = cep;
-    }
-
-    public void ImprimeAluno(List<Aluno> listaAluno) {
-        for (int i = 0; i < listaAluno.size(); i++) {
-				
-            Aluno p = (Aluno)listaAluno.get(i);
-            System.out.println(p.getAluno());
-            System.out.println(p.getDataNascimento());
-            System.out.println(p.getSexo());
-            System.out.println(p.getTelefone());
-            System.out.println(p.getCelular());
-            System.out.println(p.getEmail());
-            System.out.println(p.getObservacao());
-            System.out.println(p.getEndereco());
-            System.out.println(p.getNumero());
-            System.out.println(p.getComplemento());
-            System.out.println(p.getBairro());
-            System.out.println(p.getCidade());
-            System.out.println(p.getEstado());
-            System.out.println(p.getPais());
-            System.out.println(p.getCep());
-            System.out.println("====================");
-            //"(aluno,data_nascimento,sexo,telefone,celular,email,observacao,endereco,numero,complemento,bairro,cidade,estado,pais,cep)
-            
-        }
-    }
+  }
 }
